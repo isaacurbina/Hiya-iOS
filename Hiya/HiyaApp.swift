43e1c60f985652b00f94adfc3d56ac5a752f3e59
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2026-01-15 17:44:24 -0600

    first commit

---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Isaac Urbina on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
