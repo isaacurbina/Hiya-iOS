31a59f334904a7bb78878c423f2a99c77fd5ba71
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2026-01-15 18:12:28 -0600

    23. Lesson 2 - AI Availability Checks

--- Hiya/ContentView.swift
+++ Hiya/ContentView.swift
@@ -6,19 +6,42 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+	
+	
+	// MARK: - properties
+	
+	private var largeLanguageModel = SystemLanguageModel.default
+	@State private var response: String = ""
+
+	
+	// MARK: - body
+	
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			switch largeLanguageModel.availability {
+				case .available:
+					Text(response)
+				case .unavailable(.deviceNotEligible) :
+					Text("Unavailable: Your device isn't eligible for Apple Inteligence.")
+				case .unavailable(.appleIntelligenceNotEnabled) :
+					Text("Unavailable: Please enable Apple Intelligence in Settings")
+				case .unavailable(.modelNotReady) :
+					Text("Unavailable: The AI model isn't ready")
+				case .unavailable(let reason):
+					Text("Unavailable: The AI feature is unavailable for an unknown reason: \(String(describing: reason))")
+			}
         }
         .padding()
     }
 }
 
+
+// MARK: - preview
+
 #Preview {
     ContentView()
 }
+
